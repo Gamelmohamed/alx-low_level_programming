956679901e45721c46ecb13a33ad6b537a4fa342
Gamelmohamed <gamelmohmwd4@gmail.com>
2023-08-10 19:40:49 -0700

    gg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * return: Always 0 (success)
+*/
+
+int main(void)
+{
+    printf("with proper gramer, but the outcome is a pice of art.\n");
+    return (0);
+}
+
