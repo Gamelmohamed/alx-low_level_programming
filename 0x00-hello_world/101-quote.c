d3ffae32560cc12bcca066045393f87975e219d3
Gamelmohamed <gamelmohmwd4@gmail.com>
2023-08-12 02:30:04 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 */
 
 int main(void)
-{	char quo[] = "and that pice of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	fput("and that pice of art is useful\" - Dora Kor
+par, 2015-10-19\n",stdout);
 
 	write(1, quo, 59);
 	return (1);
