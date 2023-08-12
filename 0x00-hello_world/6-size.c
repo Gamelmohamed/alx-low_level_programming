8620a6b37df19a457d457c5b52581888352f4823
Gamelmohamed <gamelmohmwd4@gmail.com>
2023-08-12 01:03:00 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,4 +15,5 @@ printf("size of an int: %u byte(s)\n", sizeof(int));
 printf("size of a long int: %u byte(s)\n", sizeof(long int));
 printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
 printf("size of a float: %u byte(s)\n", sizeof(float));
-Return (0);
+Return(0);
+}
